41eeb79d2388b654d3727910ee1f3335a1d66a75
daminrobot <daminrobot@dmbot-mapping.local>
2025-10-15 16:43:02 +0900

    Revert "hall_testmap_9"
    This reverts commit 1a0a2937fbee3aa3dd47ed6276751f76e5210382.

--- configs/dmbot/my_cartographer_2d.lua
+++ configs/dmbot/my_cartographer_2d.lua
@@ -32,11 +32,11 @@ TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.20
-TRAJECTORY_BUILDER_2D.max_range = 15.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 15.5
+TRAJECTORY_BUILDER_2D.max_range = 10.0
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 10.15
 
 TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.02
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 180
+TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 280
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 0.45
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 10.0
 
@@ -44,7 +44,7 @@ TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 10,
 }
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
 ------------------------------------------------------------------------------------
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.06
@@ -73,16 +73,16 @@ TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 2
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true---뺄지 포함할지 고민
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.0)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(25.0)
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 0.02
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 0.08
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 0.05
 
 
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = false
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 12
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 30
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1.0e2
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 6.0e2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 4.0e2
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 6.0e1
 
 ------------------------------------------------------------------------------------
@@ -94,10 +94,10 @@ MAP_BUILDER.num_background_threads = 8
 
 -- Constraint Builder
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 8
-POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.min_score = 0.58
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.70
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.02
-POSE_GRAPH.global_sampling_ratio = 0.0005
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.global_sampling_ratio = 0.001
 
 POSE_GRAPH.optimization_problem.huber_scale = 5.0 
 POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e5
